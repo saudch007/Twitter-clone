48e9ab9663fb89a955afb683a1620fe88b483fd0
Saud <f191001@nu.edu.pk>
2023-07-30 18:10:57 +0500

    - Upload image and randomize id for each tweet in DB

--- src/app/components/Clientside/Tweet.tsx
+++ src/app/components/Clientside/Tweet.tsx
@@ -1,5 +1,10 @@
 "use client";
-import React, { ChangeEventHandler, MouseEventHandler, useState } from "react";
+import React, {
+  ChangeEvent,
+  ChangeEventHandler,
+  MouseEventHandler,
+  useState,
+} from "react";
 import supabase from "../../supabase";
 
 const Tweet = () => {
@@ -8,6 +13,7 @@ const Tweet = () => {
 
   const handleTweet: ChangeEventHandler<HTMLInputElement> = (event) => {
     event.preventDefault();
+
     setTweet(event.target.value);
   };
 
@@ -15,18 +21,34 @@ const Tweet = () => {
     event.preventDefault();
     const { error } = await supabase
       .from("tweet_table")
-      .insert({ id: TweetId, tweet: Tweet });
+      .insert({ tweet: Tweet });
 
     if (error) {
       console.log(error);
     } else {
       console.log(Tweet + "-> Tweet Successful");
-      setTweetId(TweetId + 1);
+    }
+  };
+
+  const handleUpload = async (e: ChangeEvent<HTMLInputElement>) => {
+    let file;
+
+    if (e.target.files) {
+      file = e.target.files[0];
+    }
+
+    const { data, error } = await supabase.storage
+      .from("tweet_files")
+      .upload("public" + file?.name, file as File);
+    if (data) {
+      console.log(data);
+    } else if (error) {
+      console.log(error);
     }
   };
 
   return (
-    <div className="border-t-[0.5px] px-4 border-b-[0.5px] flex items-stretch py-4  space-x-2 border-gray-600 relative">
+    <div className="border-t-[0.5px] px-4 border-b-[0.5px] flex items-stretch py-10  space-x-2 border-gray-600 relative">
       <div className="w-10 h-10 bg-slate-400 rounded-full flex-none"></div>
       <div className="flex flex-col w-full h-full">
         <input
@@ -40,15 +62,42 @@ const Tweet = () => {
         <div className=" justify-between items-center flex">
           <div></div>
 
-          <div className="w-full max-w-[100px]">
-            <button
-              onClick={handleSubmit}
-              type="submit"
-              className="w-full rounded-full bg-primary px-4 py-2 text-lg text-center font-bold hover:bg-opacity-70 transition duration-200"
-            >
-              Tweet
-            </button>
+          <div className="flex flex-row w-full max-w-[100px]">
+            <div>
+              <button
+                onClick={handleSubmit}
+                type="submit"
+                className="w-full rounded-full bg-primary mt-2 px-4 py-2 text-lg text-center font-bold hover:bg-opacity-70 transition duration-200"
+              >
+                Tweet
+              </button>
+            </div>
           </div>
+        </div>
+        <div className="outline-bg-primary hover:bg-opacity-70 transition duration-200 flex  flex-col items-center ">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+            />
+          </svg>
+          <input
+            type="file"
+            accept="image/*"
+            className=""
+            id="file_input"
+            onChange={(e) => {
+              handleUpload(e);
+            }}
+          />
         </div>
       </div>
     </div>
